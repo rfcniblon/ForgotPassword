789e120a0f8f33d17343900e905436af5fcb3b92
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2021-03-22 21:31:37 +0100

    Create App.js

---
+++ Front/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Routes from './components/Routes';
+
+const App = () => (
+  <div className="App">
+    <Routes />
+  </div>
+);
+
+export default App;
